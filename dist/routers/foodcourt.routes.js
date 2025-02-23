ca4202e2def4a5735adef7150872f5006bbe264c
Nitin Yadav <108917474+SoftwareDeveloperYadavJi@users.noreply.github.com>
2025-02-24 00:19:25 +0530

    Remodver .dist

--- dist/routers/foodcourt.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addFoodCourt, getFoodCourts, removeFoodCourt, getFoodCourtById, getTotalPendingOrders } from "../controllers/foodCourt.controller.js";
-const foodcourtRouter = Router();
-foodcourtRouter.post("/add", addFoodCourt);
-foodcourtRouter.get("/", getFoodCourts);
-foodcourtRouter.delete("/:id", removeFoodCourt);
-foodcourtRouter.get("/:id", getFoodCourtById);
-foodcourtRouter.get("/pendingorders/:id", getTotalPendingOrders);
-export default foodcourtRouter;
--- dist/routers/shopkeeper.routes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { registerShopkeeper, loginShopkeeper } from '../controllers/shopkeeper.controller.js';
-const shopkeeperRouter = express.Router();
-shopkeeperRouter.post('/register', registerShopkeeper);
-shopkeeperRouter.post('/login', loginShopkeeper);
-export default shopkeeperRouter;
